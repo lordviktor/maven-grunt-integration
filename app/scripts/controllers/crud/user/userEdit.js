3a6d13ac98ad063a785e5f7e346bc309d5738fa7
lordviktor <victorjosesilvapinto@hotmail.com>
2013-11-05 01:19:31 -0200

    Commit inicial do projeto

---
+++ app/scripts/controllers/crud/user/userEdit.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testGruntMavenApp')
+  .controller('CrudUserUsereditCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/crud/user/userList.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('testGruntMavenApp')
+  .controller('CrudUserUserlistCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
