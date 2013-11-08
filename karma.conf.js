ca2fe3a38c4fb9d30ee229162771e32090f99f14
lordviktor <victorjosesilvapinto@hotmail.com>
2013-11-08 12:41:44 -0200

    mave

--- karma.conf.js
+++ karma.conf.js
@@ -11,8 +11,8 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'app/bower_components/angular/angular.js',
-      'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/lib/angular/angular.js',
+      'app/lib/angular-mocks/angular-mocks.js',
       'app/scripts/*.js',
       'app/scripts/**/*.js',
       'test/mock/**/*.js',
